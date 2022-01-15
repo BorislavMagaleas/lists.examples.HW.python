595d5151ea13a257ef3fa09158b60cd1d820c911
BorislavMagaleas <95872181+BorislavMagaleas@users.noreply.github.com>
2022-01-15 19:43:42 +0200

    Dynamic list of clients.

--- list.simple.h.t.HW.py
+++ list.simple.h.t.HW.py
@@ -1,17 +1,19 @@
-clients             = ["John","Mary","Kate"]
-clientsHighPriority = ["Tob","Pete"]
-clientsLowPriority  = ["Vicky","Lessly"]
+clients_fin = []
 
-clientsHighPriority.reverse()
-
-def output(argument):                          ### function that prints elements of a list
+def output(argument):                                                        ### function that prints elements of a list
     for i in argument:
         print(i)
 
-for name in clients + clientsHighPriority + clientsLowPriority:
-    if name in clientsHighPriority:            ### High Priority Clients are added to the beginning of the list clients
-        clients.insert(0,name)
-    elif name in clientsLowPriority:           ### Low Priority Clients are added tot the end of the list clients
-        clients.append(name)
+seats = int(input("How many seats are available in the restaurant?"))        ### Limit of the input data is introduced
+
+for names in range(seats):
+    name = input(("What is your name?"))                                     ### User introduces his/her name
+    priority = input("What is your order's level of priority (High/Low) ?")  ### User introduces priority level of his/her order
+    if priority == "High":
+        clients_fin.insert(0,name)                                           ### High priority level -> name is added to the beginning of the list of clients 
+    elif priority == "Low":           
+        clients_fin.append(name)                                             ### Low priority level -> name is added to the end of the list of clients
+    else:
+        print("Incorrect priority level was introduced?")
     
-output(clients)                                ### Call of the function output()
+output(clients_fin)                                                          ### Call of the function output()
